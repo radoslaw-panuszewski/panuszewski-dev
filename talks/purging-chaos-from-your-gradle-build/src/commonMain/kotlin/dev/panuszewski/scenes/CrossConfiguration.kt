666e10032eb3c3955cdc767a67a68c907b384772
radoslaw.panuszewski <radoslaw.panuszewski@allegro.com>
2025-10-14 20:36:41 +0200

    Cleanup

--- talks/purging-chaos-from-your-gradle-build/src/commonMain/kotlin/dev/panuszewski/scenes/CrossConfiguration.kt
+++ talks/purging-chaos-from-your-gradle-build/src/commonMain/kotlin/dev/panuszewski/scenes/CrossConfiguration.kt
@@ -26,8 +26,6 @@ import dev.bnorm.storyboard.text.magic.splitByChars
 import dev.bnorm.storyboard.toState
 import dev.panuszewski.components.Agenda
 import dev.panuszewski.template.components.AnimatedHorizontalTree
-import dev.panuszewski.template.components.DIRECTORY
-import dev.panuszewski.template.components.EMPTY_SAMPLE
 import dev.panuszewski.template.components.IdeLayout
 import dev.panuszewski.template.components.MagicAnnotatedString
 import dev.panuszewski.template.components.TitleScaffold
@@ -35,7 +33,6 @@ import dev.panuszewski.template.components.buildCodeSamples
 import dev.panuszewski.template.components.buildIdeState
 import dev.panuszewski.template.components.buildTree
 import dev.panuszewski.template.components.calculateTotalStates
-import dev.panuszewski.template.components.initiallyHidden
 import dev.panuszewski.template.extensions.SlideOutToBottomAnimatedVisibility
 import dev.panuszewski.template.extensions.annotate
 import dev.panuszewski.template.extensions.startWith
@@ -48,16 +45,7 @@ import dev.panuszewski.template.theme.withColor
 
 fun StoryboardBuilder.CrossConfiguration() {
     val files = listOf(
-        "build.gradle.kts" to BUILD_GRADLE_KTS,
-        "lib1" to DIRECTORY.initiallyHidden(),
-        "lib1/build.gradle.kts" to EMPTY_SAMPLE.initiallyHidden(),
-        "lib2" to DIRECTORY.initiallyHidden(),
-        "lib2/build.gradle.kts" to EMPTY_SAMPLE.initiallyHidden(),
-        "app" to DIRECTORY.initiallyHidden(),
-        "libre-office-installer" to DIRECTORY.initiallyHidden(),
-        "buildSrc" to DIRECTORY.initiallyHidden(),
-        "buildSrc/src/main/kotlin" to DIRECTORY.initiallyHidden(),
-        "buildSrc/src/main/kotlin/wtf-lib.gradle.kts" to WTF_LIB_GRADLE_KTS.initiallyHidden(),
+        "build.gradle.kts" to BUILD_GRADLE_KTS
     )
 
     val totalStates = calculateTotalStates(files)
@@ -84,38 +72,6 @@ fun StoryboardBuilder.CrossConfiguration() {
                             val highlightColor = NICE_ORANGE
 
                             val tree = when {
-//                                panelState.currentState >= 7 -> buildTree {
-//                                    node("root-project", rootProjectColor) {
-//                                        node("lib1", libraryColor)
-//                                        node("lib2", libraryColor)
-//                                    }
-//                                }
-//                                panelState.currentState >= 6 -> buildTree {
-//                                    node("root-project", rootProjectColor) {
-//                                        node("lib1", libraryColor)
-//                                        node("lib2", libraryColor)
-//                                        node("app", appColor)
-//                                        node("libre-office-installer", appColor)
-//                                    }
-//                                }
-//                                panelState.currentState >= 5 -> buildTree {
-//                                    node("root-project", rootProjectColor) {
-//                                        node("lib1", libraryColor)
-//                                        node("lib2", libraryColor)
-//                                        node("app", appColor)
-//                                    }
-//                                }
-//                                panelState.currentState >= 4 -> buildTree {
-//                                    node("root-project", rootProjectColor) {
-//                                        node("lib1", libraryColor)
-//                                        node("lib2", libraryColor)
-//                                    }
-//                                }
-//                                panelState.currentState >= 3 -> buildTree {
-//                                    node("root-project", rootProjectColor) {
-//                                        node("lib1", libraryColor)
-//                                    }
-//                                }
                                 panelState.currentState == 12 -> buildTree {
                                     node("root-project", rootProjectColor) {
                                         node("app", appColor)
@@ -259,23 +215,8 @@ private val ROOT_BUILD_GRADLE_KTS = buildCodeSamples {
 }
 
 private val BUILD_GRADLE_KTS = buildCodeSamples {
-    val appConfig by tag()
-    val subprojectsBlock by tag()
-    val javaLibrary by tag()
-    val mavenPublish by tag()
-    val publication by tag()
-    val subprojectsFilter by tag()
-    val subprojectsForEach by tag()
-    val subprojectsIndent1 by tag()
-    val subprojectsIndent2 by tag()
-    val subprojectsIndent3 by tag()
-    val subprojectsIndent4 by tag()
-    val subprojectsIndent5 by tag()
-    val subprojectsClosingBrace by tag()
-    val commonConfig by tag()
-
     """
-    ${appConfig}plugins {
+    plugins {
         `wtf-app`
     }
     
@@ -283,75 +224,12 @@ private val BUILD_GRADLE_KTS = buildCodeSamples {
         implementation(projects.subProject)
         implementation(libs.spring.boot.web)
     }
-        
-    ${appConfig}${subprojectsBlock}subprojects${subprojectsFilter}
-        .filter { it.name.startsWith("lib")${subprojectsFilter}${subprojectsForEach}
-        .forEach${subprojectsForEach} { ${javaLibrary}${commonConfig}
-        ${subprojectsIndent1}    it.${subprojectsIndent1}apply(plugin = "java-library")
-    ${javaLibrary}${mavenPublish}    ${subprojectsIndent2}    it.${subprojectsIndent2}apply(plugin = "maven-publish")${publication}
-    
-        ${subprojectsIndent3}    ${subprojectsIndent3}publishing.publications.create<MavenPublication>("library") {
-        ${subprojectsIndent4}    ${subprojectsIndent4}    from(components["java"])
-        ${subprojectsIndent5}    ${subprojectsIndent5}}${publication}${commonConfig}${subprojectsClosingBrace}
-        }${subprojectsClosingBrace}
-    ${mavenPublish}}${subprojectsBlock}
     """
         .trimIndent()
         .toCodeSample(language = Language.KotlinDsl)
-        .startWith { hide(subprojectsBlock, mavenPublish, publication, subprojectsFilter, subprojectsForEach, subprojectsIndent1, subprojectsIndent2, subprojectsIndent3, subprojectsIndent4, subprojectsIndent5, subprojectsClosingBrace) }
+        .startWith { this }
         .openPanel("tree")
         .pass()
         .hideIde()
         .pass(100)
-        .revealFile("app")
-        // ---
-        .pass()
-        .revealFile("lib1")
-        .revealFile("lib2")
-        .closePanel("tree")
-        .then { reveal(mavenPublish) }
-        .then { reveal(publication) }
-        .openPanel("tree")
-        .revealFile("app")
-        .closePanel("tree")
-        .then { reveal(subprojectsFilter, subprojectsForEach, subprojectsIndent1, subprojectsIndent2, subprojectsIndent3, subprojectsIndent4, subprojectsIndent5, subprojectsClosingBrace).focus(subprojectsFilter) }
-        .openPanel("tree")
-        .revealFile("libre-office-installer")
-        .then { unfocus().closePanel("tree") }
-        .openInRightPane("buildSrc/src/main/kotlin/wtf-lib.gradle.kts", switchTo = true)
-        .then { focus(commonConfig) }
-        .then { hide(commonConfig) }
-        .then { hide(subprojectsBlock) }
-}
-
-private val WTF_LIB_GRADLE_KTS = buildCodeSamples {
-    val config by tag()
-    val todo by tag()
-
-    """
-    ${config}plugins {
-        `java-library`    
-        `maven-publish`
-    }
-    
-    publishing.publications.create<MavenPublication>("library") {
-        from(components["java"])
-    }
-    
-    ${config}${todo}// TODO${todo}
-    """
-        .trimIndent()
-        .toCodeSample(language = Language.KotlinDsl)
-        .startWith { hide(config) }
-        .hideFileTree()
-        .thenTogetherWith("build.gradle.kts") { this }
-        .thenTogetherWith("build.gradle.kts") { reveal(config) }
-        .thenTogetherWith("build.gradle.kts") { hide(todo) }
-        .then { showFileTree().closeLeftPane().hideFile("app").hideFile("libre-office-installer").resumePanel("tree") }
-        .openPanel("tree")
-        .pass()
-        .closePanel("tree")
-        .openAgenda()
-        .pass()
-        .closeAgenda()
 }
